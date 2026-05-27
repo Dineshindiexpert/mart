22194c13849a8c6816ee803d2ffb3fbd4061fed5
dineshindixpert <dineshjangra0062@gmail.com>
2026-05-27 13:09:16 +0530

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import router from './routes/AppRoutes';
+import router from './routes/AppRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <RouterProvider router={router} />
-
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/AppRoutes";
+import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom.css';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <App />
 );
